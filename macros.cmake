c22ceca45ffb02fcce27766a6afd3e5e977c0fe7
Deepak Sirone Jegan <dsirone@cs.wisc.edu>
2022-07-05 18:10:45 -0500

    Init commit; working cert verif callback

---
+++ macros.cmake
@@ -0,0 +1,205 @@
+cmake_minimum_required(VERSION 3.10)
+include(ExternalProject)
+
+macro(global_set Name Value)
+    #  message("set ${Name} to " ${ARGN})
+    set(${Name} "${Value}" CACHE STRING "NoDesc" FORCE)
+endmacro()
+
+macro(use_riscv_toolchain bits)
+  set(cross_compile riscv${bits}-unknown-linux-gnu-)
+  execute_process(
+    COMMAND which ${cross_compile}gcc
+    OUTPUT_VARIABLE CROSSCOMPILE
+    RESULT_VARIABLE ERROR)
+
+  if (NOT "${ERROR}" STREQUAL 0)
+    message(FATAL_ERROR "RISCV Toochain is not found")
+  endif()
+
+  string(STRIP ${CROSSCOMPILE} CROSSCOMPILE)
+  string(REPLACE "gcc" "" CROSSCOMPILE ${CROSSCOMPILE})
+
+  message(STATUS "Tagret tripplet: ${CROSSCOMPILE}")
+
+  set(CC              ${CROSSCOMPILE}gcc)
+  set(CXX             ${CROSSCOMPILE}g++)
+  set(LD              ${CROSSCOMPILE}ld)
+  set(AR              ${CROSSCOMPILE}ar)
+  set(OBJCOPY         ${CROSSCOMPILE}objcopy)
+  set(OBJDUMP         ${CROSSCOMPILE}objdump)
+  set(CFLAGS          "-Wall -Werror")
+
+  global_set(CMAKE_C_COMPILER        ${CC}${EXT})
+  global_set(CMAKE_ASM_COMPILER        ${CC}${EXT})
+  global_set(CMAKE_CXX_COMPILER      ${CXX}${EXT})
+  global_set(CMAKE_LINKER            ${LD}${EXT})
+  global_set(CMAKE_AR                ${AR}${EXT})
+  global_set(CMAKE_OBJCOPY           ${OBJCOPY}${EXT})
+  global_set(CMAKE_OBJDUMP           ${OBJDUMP}${EXT})
+  global_set(CMAKE_C_FLAGS           ${CMAKE_C_FLAGS} ${CFLAGS})
+
+  check_compiler(${CMAKE_C_COMPILER})
+  check_compiler(${CMAKE_CXX_COMPILER})
+
+  global_set(CMAKE_C_COMPILER_WORKS      1)
+  global_set(CMAKE_CXX_COMPILER_WORKS    1)
+
+  global_set(CMAKE_SYSTEM_NAME    "Linux")
+
+endmacro()
+
+macro(use_riscv_musl_toolchain bits)
+  set(cross_compile riscv${bits}-unknown-linux-musl-)
+  execute_process(
+    COMMAND which ${cross_compile}gcc
+    OUTPUT_VARIABLE CROSSCOMPILE
+    RESULT_VARIABLE ERROR)
+
+  if (NOT "${ERROR}" STREQUAL 0)
+    message(FATAL_ERROR "RISCV Toochain is not found")
+  endif()
+
+  string(STRIP ${CROSSCOMPILE} CROSSCOMPILE)
+  string(REPLACE "gcc" "" CROSSCOMPILE ${CROSSCOMPILE})
+
+  message(STATUS "Tagret tripplet: ${CROSSCOMPILE}")
+
+  set(CC              ${CROSSCOMPILE}gcc)
+  set(CXX             ${CROSSCOMPILE}g++)
+  set(LD              ${CROSSCOMPILE}ld)
+  set(AR              ${CROSSCOMPILE}ar)
+  set(OBJCOPY         ${CROSSCOMPILE}objcopy)
+  set(OBJDUMP         ${CROSSCOMPILE}objdump)
+  set(CFLAGS          "-Wall -Werror")
+
+  global_set(CMAKE_C_COMPILER        ${CC}${EXT})
+  global_set(CMAKE_ASM_COMPILER        ${CC}${EXT})
+  global_set(CMAKE_CXX_COMPILER      ${CXX}${EXT})
+  global_set(CMAKE_LINKER            ${LD}${EXT})
+  global_set(CMAKE_AR                ${AR}${EXT})
+  global_set(CMAKE_OBJCOPY           ${OBJCOPY}${EXT})
+  global_set(CMAKE_OBJDUMP           ${OBJDUMP}${EXT})
+  global_set(CMAKE_C_FLAGS           ${CMAKE_C_FLAGS} ${CFLAGS})
+
+  check_compiler(${CMAKE_C_COMPILER})
+  check_compiler(${CMAKE_CXX_COMPILER})
+
+  global_set(CMAKE_C_COMPILER_WORKS      1)
+  global_set(CMAKE_CXX_COMPILER_WORKS    1)
+
+  global_set(CMAKE_SYSTEM_NAME    "Linux")
+
+endmacro()
+
+
+macro(check_compiler target)
+  message(STATUS "Check for working C compiler: ${target}")
+  execute_process(
+    COMMAND ${target} -print-file-name=crt.o
+    OUTPUT_FILE OUTPUT
+    RESULT_VARIABLE ERROR)
+
+  if ("${ERROR}" STREQUAL 0)
+    message(STATUS "Check for working C compiler: ${target} -- works")
+  else()
+    message(FATAL_ERROR "Check for working C compiler: ${target} -- not working")
+  endif()
+endmacro()
+
+macro(subdirs_list OUT_VARIABLE DIRWORK)
+  file(GLOB children RELATIVE ${DIRWORK} ${DIRWORK}/*)
+  set(_subdirs    "")
+  foreach(child ${children})
+    if (IS_DIRECTORY ${DIRWORK}/${child})
+      list(APPEND _subdirs  ${child})
+    endif()
+  endforeach()
+  set(${OUT_VARIABLE} ${_subdirs})
+endmacro()
+
+macro(add_files FILE_LIST DIRWORK)
+  foreach(file ${ARGN})
+    list(APPEND ${FILE_LIST} ${DIRWORK}/${file})
+  endforeach()
+endmacro()
+
+# CMake macro for Eyrie runtime and Keystone Package
+macro(add_eyrie_runtime target_name tag plugins) # the files are passed via ${ARGN}
+  set(runtime_prefix runtime)
+  set (eyrie_src ${CMAKE_CURRENT_BINARY_DIR}/${runtime_prefix}/src/eyrie-${target_name})
+
+  ExternalProject_Add(eyrie-${target_name}
+    PREFIX ${runtime_prefix}
+    GIT_REPOSITORY git@github.com:deepaksirone/keystone-runtime.git
+    GIT_TAG ${tag}
+    CONFIGURE_COMMAND ""
+    UPDATE_COMMAND git pull origin tap
+    BUILD_COMMAND ./build.sh ${plugins}
+    BUILD_IN_SOURCE TRUE
+    INSTALL_COMMAND "")
+
+  add_custom_target(${target_name} DEPENDS ${ARGN})
+
+  foreach(item IN ITEMS ${ARGN})
+    add_custom_command(OUTPUT ${item} DEPENDS eyrie-${target_name} ${eyrie_src}/${item}
+      COMMAND cp ${eyrie_src}/${item} ${item})
+  endforeach(item)
+
+endmacro(add_eyrie_runtime)
+
+macro(add_wolfssl target_name tag libc plugins)
+  set (tls_prefix tls)       
+  set (wolfssl_src ${CMAKE_CURRENT_BINARY_DIR}/${tls_prefix}/src/wolfssl-${target_name})
+  set (wolfssl_install ${CMAKE_CURRENT_BINARY_DIR}/${tls_prefix}/install)
+
+  ExternalProject_Add(wolfssl-${target_name}
+    PREFIX ${tls_prefix}
+    INSTALL_DIR ${tls_prefix}/install
+    GIT_REPOSITORY git@github.com:deepaksirone/wolfssl.git
+    GIT_TAG ${tag}
+    CONFIGURE_COMMAND ./autogen.sh && ./configure --prefix=${wolfssl_install} --exec-prefix=${wolfssl_install} --enable-filesystem=no --enable-certgen --enable-certreq --enable-keygen --enable-sessioncerts --enable-certext --enable-asn=template --enable-static --disable-shared --enable-harden --enable-singlethreaded
+    UPDATE_COMMAND git checkout ${tag}
+    BUILD_COMMAND make install EXTRA_CFLAGS=${plugins}
+    BUILD_IN_SOURCE TRUE
+    INSTALL_COMMAND ""
+  )
+
+  add_custom_target(${target_name} DEPENDS wolfssl-${target_name})
+  include_directories(AFTER ${wolfssl_install}/include)
+  set(WOLFSSL_LIB ${wolfssl_install}/lib/libwolfssl.a)
+
+endmacro(add_wolfssl) 
+  
+
+macro(add_keystone_package target_name package_name package_script) # files are passed via ${ARGN}
+  set(pkg_dir ${CMAKE_CURRENT_BINARY_DIR}/pkg)
+  add_custom_command(OUTPUT ${pkg_dir} COMMAND mkdir ${pkg_dir})
+
+  message(STATUS " * Configuring Keystone package (${target_name})")
+  foreach(dep IN ITEMS ${ARGN})
+    get_filename_component(filename ${dep} NAME)
+    string(CONCAT pkg_file "${pkg_dir}/" "${filename}")
+    list(APPEND pkg_files ${pkg_file})
+
+    message(STATUS "   Adding ${filename}")
+    add_custom_command(OUTPUT ${pkg_file} DEPENDS ${dep} ${pkg_dir}
+      COMMAND cp ${dep} ${pkg_file})
+  endforeach(dep)
+
+  message(STATUS "   Package: ${package_name}")
+  message(STATUS "   Script: ${package_script}")
+
+  separate_arguments(package_script_raw UNIX_COMMAND ${package_script})
+  add_custom_target(${target_name} DEPENDS ${pkg_files}
+    COMMAND
+      ${MAKESELF} --noprogress ${pkg_dir} ${package_name} \"Keystone Enclave Package\" ${package_script_raw}
+    )
+
+endmacro(add_keystone_package)
+
+macro(add_cryptor binary_target)
+	add_custom_target(${binary_target}-cryptor 
+		DEPENDS ${binary_target}
+		COMMAND python3 /data/projects/phd_stuff/tap/cryptor/cryptor.py ${binary_target})
+endmacro(add_cryptor)
---
+++ tap-client.c
@@ -0,0 +1,214 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+
+#include <wolfssl/options.h>
+#include <wolfssl/wolfcrypt/settings.h>
+#include <wolfssl/certs_test.h>
+#include <wolfssl/ssl.h>
+#include <wolfssl/wolfcrypt/types.h>
+#include <wolfssl/wolfcrypt/aes.h>
+#include <wolfssl/wolfcrypt/asn.h>
+
+void usage() {
+	printf("./tap_client <request> <request_params>");
+}
+
+#define derBufSz 8000
+
+int myCustomExtCallback(const word16* oid, word32 oidSz, int crit,
+                               const unsigned char* der, word32 derSz) {
+    word32 i;
+
+    printf("Custom Extension found!\n");
+    printf("(");
+    for (i = 0; i < oidSz; i++) {
+        printf("%d", oid[i]);
+        if (i < oidSz - 1) {
+            printf(".");
+        }
+    }
+    printf(") : ");
+
+    if (crit) {
+        printf("CRITICAL");
+    } else {
+        printf("NOT CRITICAL");
+    }
+    printf(" : ");
+
+    for (i = 0; i < derSz; i ++) {
+        printf("%x ", der[i]);
+    }
+    printf("\n");
+    fflush(stdout);
+
+    /* NOTE: by returning zero, we are accepting this extension and informing
+     *       wolfSSL that it is acceptable. If you find an extension that you
+     *       do not find acceptable, you should return an error. The standard 
+     *       behavior upon encountering an unknown extension with the critical
+     *       flag set is to return ASN_CRIT_EXT_E. For the sake of brevity,
+     *       this example is always accepting every extension; you should use
+     *       different logic. */
+    return 0;
+}
+
+
+int verify_attested_tls(int preverify, WOLFSSL_X509_STORE_CTX* store_ctx) {
+    printf("[verify_attested_tls] Entering\n");
+	WOLFSSL_X509 *current_cert = store_ctx->current_cert;
+	DecodedCert *decodedCert = (DecodedCert *)malloc(sizeof(DecodedCert));
+    //char *derbuf = (char *)malloc(8000 * sizeof(char));
+
+    unsigned char *derBuffer[1];
+    derBuffer[0] = NULL;
+
+    int derSz = wolfSSL_i2d_X509(current_cert, derBuffer);
+    fflush(stdout);
+    wc_InitDecodedCert(decodedCert, derBuffer[0], derSz, 0);
+    
+
+    int ret = wc_SetUnknownExtCallback(&decodedCert, myCustomExtCallback);
+
+    ret = ParseCert(decodedCert, CERT_TYPE, NO_VERIFY, NULL);
+    if (ret == 0) {
+        printf("[verify_attested_tls] Cert issuer: %s\n", decodedCert->issuer);
+    }
+    
+    return (ret == ASN_CRIT_EXT_E) ? 0 : 1;
+}
+
+WOLFSSL* Client(WOLFSSL_CTX* ctx, char* suite, int setSuite)
+{
+    WOLFSSL* ssl = NULL;
+    int ret;
+
+    if ((ctx = wolfSSL_CTX_new(wolfTLSv1_2_client_method())) == NULL) {
+        printf("Error in setting client ctx\n");
+        return NULL;
+    }
+
+    wolfSSL_CTX_set_verify(ctx, SSL_VERIFY_PEER, verify_attested_tls);
+    //wolfSSL_CTX_set_verify(ctx, SSL_VERIFY_NONE, 0);
+
+    if (setSuite == 1) {
+        if ((ret = wolfSSL_CTX_set_cipher_list(ctx, suite)) != SSL_SUCCESS) {
+            printf("ret = %d\n", ret);
+            printf("can't set cipher\n");
+            wolfSSL_CTX_free(ctx);
+            return NULL;
+        }
+    } else {
+        (void) suite;
+    }
+
+    //wolfSSL_SetIORecv(ctx, CbIORecv);
+    //wolfSSL_SetIOSend(ctx, CbIOSend);
+
+    if ((ssl = wolfSSL_new(ctx)) == NULL) {
+        printf("issue when creating ssl\n");
+        wolfSSL_CTX_free(ctx);
+        return NULL;
+    }
+
+    return ssl;
+}
+
+int32_t initiate_connection(char *hostname, int32_t port) {
+
+	int fd_sock;
+	struct sockaddr_in server_addr;
+	struct hostent *hostnm;
+
+	fd_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (fd_sock < 0) {
+		printf("[init] Failed to open socket\n");
+		exit(-1);
+	}
+
+	memset(&server_addr, 0, sizeof(server_addr));
+	hostnm = gethostbyname(hostname);
+
+	if (hostnm == NULL) {
+		printf("[init] Gethostname failed");
+		exit(-1);
+	}
+
+	server_addr.sin_family      = AF_INET;
+    server_addr.sin_port        = htons(port);
+    server_addr.sin_addr = *((struct in_addr *)hostnm->h_addr_list[0]);
+
+	if (connect(fd_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0 ) {
+		printf("[init] connect error");
+		exit(-1);
+	}
+
+	return fd_sock;
+}
+
+int main(int argc, char **argv)
+{
+	char msg[] = "GET / HTTP/1.1\r\nConnection: close\r\n\r\n";
+    //Not working!: 
+    //char msg[] = "GET / HTTP/1.1\r\n";
+    char reply[65535];
+    int ret, msgSz, sockfd;
+    struct sockaddr_in servAddr;
+    WOLFSSL* sslCli;
+    WOLFSSL_CTX* ctxCli = NULL;
+    msgSz = strlen(msg);
+
+    wolfSSL_Init();
+
+    char *hostname = "google.com";
+    int host_len = strlen(hostname);
+
+    sslCli = Client(ctxCli, "let-wolfssl-decide", 0);
+    sockfd = initiate_connection(hostname, 443);
+
+    /* Attach wolfSSL to the socket */
+    if ((ret = wolfSSL_set_fd(sslCli, sockfd)) != WOLFSSL_SUCCESS) {
+        fprintf(stderr, "ERROR: Failed to set the file descriptor\n");
+        goto cleanup;
+    }
+
+    ret = SSL_FAILURE;
+
+    printf("Starting client\n");
+    while (ret != SSL_SUCCESS) {
+        int error;
+        printf("Connecting..\n");
+        /* client connect */
+        ret |= wolfSSL_connect(sslCli);
+        error = wolfSSL_get_error(sslCli, 0);
+        if (ret != SSL_SUCCESS) {
+            if (error != SSL_ERROR_WANT_READ &&
+                error != SSL_ERROR_WANT_WRITE) {
+                printf("client ssl connect failed\n");
+                goto cleanup;
+            }
+        }
+        printf("Client connected successfully...\n");
+    }
+
+
+
+cleanup:
+    wolfSSL_free(sslCli);      /* Free the wolfSSL object                  */
+ctx_cleanup:
+    wolfSSL_CTX_free(ctxCli);  /* Free the wolfSSL context object          */
+    wolfSSL_Cleanup();      /* Cleanup the wolfSSL environment          */
+socket_cleanup:
+    close(sockfd);          /* Close the connection to the server       */
+end:
+    return ret;               /* Return reporting a success               */
+    
+}
